3ab9a34c6328fcebfb422b0cec92faae59a9bd54
heycn <heycn@foxmail.com>
2023-04-18 11:38:05 +0800

    添加获取 VS Code 所有扩展脚本

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 [`.vscode/settings.json`](./.vscode/settings.json)<br>
 [`.vscode/extensions.json`](./.vscode/extensions.json)<br>
 [`.vscode/global.code-snippets`](./.vscode/global.code-snippets)
+
+## 脚本
+
+### 获取 VS Code 所有扩展
+
+```bash
+chmod +x bin/exts_list.sh
+
+bin/exts_list.sh
+```
---
+++ bin/exts_list.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+rm .vscode/my_vscode_exts.sh
+echo "# Generated item: $timestamp \n" > .vscode/my_vscode_exts.sh
+code --list-extensions | sed -e 's/^/code --install-extension /' >> .vscode/my_vscode_exts.sh
+echo ".vscode/my_vscode_exts.sh"
